cd9ff1a6fe9d95cbc8667f0ac3c358064c6d5fb0
McCambley <jakemccamb16@gmail.com>
2022-01-15 18:06:39 -0500

    Dev: initialize

---
+++ pages/posts/first-post.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/Layout'
+export default function FirstPost() {
+    return (
+        <>
+            <Layout>
+                <Head>
+                    <title>First Post</title>
+                </Head>
+                <h1>First Post</h1>
+                <Link href='/'><a>Back to home</a></Link>
+            </Layout>
+        </>
+    )
+}
