71a405bc378b7701f9b1ba63830149ce6802af88
McCambley <jakemccamb16@gmail.com>
2022-01-15 22:57:26 -0500

    Ref: replace static post with dynamic post

---
+++ pages/posts/[id].js
@@ -0,0 +1,9 @@
+import Layout from "../../components/layout";
+
+export default function Post() {
+  return <Layout>...</Layout>;
+}
+
+export async function getStaticPaths() {
+  // return a list of possible values for id
+}
