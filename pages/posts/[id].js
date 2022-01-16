f54b6d53362bcb51a71f3a3f041ec92d79a778bf
McCambley <jakemccamb16@gmail.com>
2022-01-16 10:08:50 -0500

    Fix: fix imports

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,4 @@
-import Layout from "../../components/layout";
+import Layout from "../../components/Layout";
 import Head from "next/head";
 import Date from "../../components/Date";
 import { getAllPostIds, getPostData } from "../../lib/posts";
