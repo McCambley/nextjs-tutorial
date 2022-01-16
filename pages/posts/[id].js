a62130c5ba636da30d83092c99488afe73f3106b
McCambley <jakemccamb16@gmail.com>
2022-01-16 09:29:29 -0500

    Dev: render html

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,6 +9,7 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
     </Layout>
   );
 }
@@ -23,7 +24,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
