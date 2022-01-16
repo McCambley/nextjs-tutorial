c90789b7801cbb7e90f173337da0010f3ab71fef
McCambley <jakemccamb16@gmail.com>
2022-01-16 09:20:44 -0500

    Dev: generate dynamic routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,32 @@
 import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
   // return a list of possible values for id
+  const paths = getAllPostIds();
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
