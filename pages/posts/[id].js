8f97db3bb44e28097b961c708543dcef1d12274f
McCambley <jakemccamb16@gmail.com>
2022-01-16 09:37:51 -0500

    Dev: formate title and date

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,20 @@
 import Layout from "../../components/layout";
+import Head from "next/head";
+import Date from "../../components/Date";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
+      <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
